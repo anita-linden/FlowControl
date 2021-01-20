b5ab4e21c2ff226a79b70897f40dd23b4839bd84
anita <anita.marjatta.linden@gmail.com>
2021-01-20 17:00:05 +0100

    Menyval 1 halvklart

--- FlowControl/Program.cs
+++ FlowControl/Program.cs
@@ -16,6 +16,9 @@ namespace FlowControl
 
                 switch (input)
                 {
+                    case "1":
+                        BuyTickets();
+                        break;
                     case "0":
                         Console.WriteLine("Good bye!");
                         running = false;
@@ -26,12 +29,31 @@ namespace FlowControl
                 }
             }
 
-            static void PrintInstructions()
-            {
-                Console.WriteLine("Welcome to the main menu. Please input a number to access different operations.\n");
-                Console.WriteLine("0 - Exits the program");
-                Console.WriteLine("\n");
-            }
+
+            
         }
+        private static void PrintInstructions()
+        {
+            Console.WriteLine("Välkommen till huvudmenyn. Vänligen ange numret för vald funktion.\n");
+            Console.WriteLine("1 - Biljett system");
+            Console.WriteLine("0 - Avsluta program");
+            Console.WriteLine("\n");
+        }
+
+        private static void BuyTickets()
+        {
+            Console.WriteLine("Hur gammal är kunden?");
+            int age;
+            while (!int.TryParse(Console.ReadLine(), out age))
+                Console.WriteLine("Please type a number for age");
+
+            if (age < 20)
+                Console.WriteLine("Ungdomspris: 80 kr");
+            else if (age > 64)
+                Console.WriteLine("Pensionärspris: 90 kr");
+            else
+                Console.WriteLine("Standardpris: 120 kr");
+        }
+
     }
 }
