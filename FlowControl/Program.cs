c854d6dd066edb16bfd5b2b9c1b2a8716b739b8b
anita <anita.marjatta.linden@gmail.com>
2021-01-20 16:40:58 +0100

    Add project files.

---
+++ FlowControl/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FlowControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool running = true;
+
+            while(running)
+            {
+                PrintInstructions();
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "0":
+                        Console.WriteLine("Good bye!");
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Undefined input.\n");
+                        break;
+                }
+            }
+
+            static void PrintInstructions()
+            {
+                Console.WriteLine("Welcome to the main menu. Please input a number to access different operations.\n");
+                Console.WriteLine("0 - Exits the program");
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
